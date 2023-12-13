a1b533239c97c15ee079916726cadd5ea80be30b
chidopraise <abrahamlivinus@gmail.com>
2023-12-13 06:02:48 -0800

    kelab_atoi.c

--- kelab_atoi.c
+++ kelab_atoi.c
@@ -1,10 +1,11 @@
 #include "shell.h"
 
 /**
-* interactive - returns true if shell is interactive mode
-* @info: struct address
+* interactive - This function returns true
+* if shell is interactive mode
+* @info: collects address info.
 *
-* Return: 1 if interactive mode, 0 otherwise
+* Return: 1 if in an interactive mode, 0 otherwise
 */
 int interactive(info_t *info)
 {
@@ -12,9 +13,9 @@ return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
+ * is_delim - This function checks if character is a delimeter
+ * @c: character to check
+ * @delim: delimeter string
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
@@ -26,9 +27,9 @@ return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
+ * _isalpha - this function checks for alphabetic character
+ * @c: character to input
+ * Return: 1 if c is alphabetic, 0 else
  */
 
 int _isalpha(int c)
@@ -40,9 +41,9 @@ return (0);
 }
 
 /**
-* _atoi - converts a string to an integer
-* @s: the string to be converted
-* Return: 0 if no numbers in string, converted number otherwise
+* _atoi - This function converts a string to an integer
+* @s: string to be converted
+* Return: 0 if Zero numbers in string, converted number otherwise
 */
 
 int _atoi(char *s)
