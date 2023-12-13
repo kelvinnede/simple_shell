b2b6b8bb19f9765843060a1c0e8426d52df363a2
Kelvin Nede <kelvinnede@gmail.com>
2023-12-13 04:25:18 -0800

    tokenize check

--- tokenizer.c
+++ tokenizer.c
@@ -63,7 +63,7 @@ if (str == NULL || str[0] == 0)
 return (NULL);
 for (i = 0; str[i] != '\0'; i++)
 if ((str[i] != d && str[i + 1] == d) ||
-    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+(str[i] != d && !str[i + 1]) || str[i + 1] == d)
 numwords++;
 if (numwords == 0)
 return (NULL);
